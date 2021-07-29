a85dedbf34a279acc987b63c83ed96685b8c2063
kujhin <70690205+kujhin@users.noreply.github.com>
2021-07-29 15:23:12 +0900

    Update AutoTrade.py

--- AutoTrade.py
+++ AutoTrade.py
@@ -5,19 +5,19 @@ import datetime
 access = "access"
 secret = "secret"   
 
-# 매매하고자 하는 코인
-# coin = "KRW-ETH"
+
+myCoin = "KRW-ETH"
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=20)
-    target_price = df['close'] + (df['high'] - df['low']) * k
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=10)
+    target_price = df.iloc[0]['close'] + (df.iloc[0]['high'] - df.iloc[0]['low']) * k
     return target_price
 
 def get_start_time(ticker):
     """시작 시간 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="minute240", count=1)
-    start_time = df.index
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=1)
+    start_time = df.index[0]
     return start_time
 
 def get_balance(ticker):
@@ -39,27 +39,26 @@ def get_current_price(ticker):
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
 
-# # 자동매매 시작
+# 자동매매 시작
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC")  # 09:00
-        end_time = start_time + datetime.timedelta(minutes=240)  # 09:00 + 1일
+        start_time = get_start_time(myCoin)  # 09:00
+        end_time = start_time + datetime.timedelta(days=1)  # 09:00 + 1일
 
         # 9:00 < 현재 < #8:59:50
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-ETH", 0.5)[get_target_price("KRW-ETH", 0.5).index == start_time[0]][0]
-            current_price = get_current_price("KRW-ETH")
+            target_price = get_target_price(myCoin, 0.5)
+            current_price = get_current_price(myCoin)
             if target_price < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-ETH", krw*0.9995)
+                    upbit.buy_market_order(myCoin, krw*0.9995)
         else:
-            btc = get_balance("ETH")
-            if btc > 0:
-                upbit.sell_market_order("KRW-ETH", btc*0.9995)
+            btc = get_balance(myCoin.split("-")[1])
+            if btc > 0.0000008:
+                upbit.sell_market_order(myCoin, btc*0.9995)
         time.sleep(1)
     except Exception as e:
         print(e)
         time.sleep(1)
-
